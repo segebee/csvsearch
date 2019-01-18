96159de4106e14161c7ebcb6eef4023dcb0dbe8d
segebee <segebee@gmail.com>
2019-01-18 17:25:49 +0100

    added streams for csv parsing

--- src/server/public/javascripts/app.js
+++ src/server/public/javascripts/app.js
@@ -8,100 +8,38 @@ const searchItem = ".search-result-item";
 const resultsList = "#resultsList";
 const resultDetails = "#resultDetails";
 const uploadForm = "#uploadForm";
+const uploadField = "uploadField";
+const uploadMessage = "#uploadMessage";
+const progressContainer = ".progress";
+const progressBar = document.getElementById("progressbar");
 
-// is document ready
-$(document).ready(function() {
-  // define constants
-
-  // handle search on keyup
-  $(searchField).on("keyup paste", function() {
-    // clear results list
-    // $(resultsList).html("");
-    const keyword = $(this).val();
-    if (keyword.length < 1) {
-      $(resultsList).slideUp();
-      // .empty();
-      return false;
-    }
-    console.log({ keyword });
-    // search db with keyword
-    searchPersons(searchPersonsUrl, keyword);
-  });
-
-  // handle search result selection
-  $(searchItem).on("click", function() {
-    // hide autocomplete
-    $(resultsList).slideUp("slow");
-    // get data
-    const data = $(this).data();
-    console.log({ data });
-    // render data
-    const renderedHtml = renderResultDetails(data);
-    console.log({ renderedHtml });
-    // display data
-    $(resultDetails).html(renderedHtml);
-  });
-
-  // handle uploads
-  $(uploadForm).on("submit", function(event) {
-    event.preventDefault();
-    // shift focus to the search box
-    $(searchField).focus();
-  });
-});
-
-// fetch the list of persons
-// function fetchAllPersons(getPersonsUrl) {
-//   let persons = [];
-//   $.getJSON(getPersonsUrl, function(resp) {
-//     console.log({ resp });
-//     persons = resp.data;
-//   });
-// }
-
-// upload csv
-function createPersons(createPersonsUrl) {
-  let persons = [];
-  $.getJSON(createPersonsUrl, function(resp) {
-    console.log({ resp });
-    persons = resp.data;
-  });
+// display upload progress
+function showProgress(evt) {
+  // update progressBar width
+  return (progressBar.style.width = (evt.loaded / evt.total) * 100 + "%");
 }
-
-// fetch the list of persons
-function searchPersons(searchPersonsUrl, keyword) {
-  let persons = [];
+// fetch the list of persons using keyword
+function searchPersons(keyword) {
   const searchRequest = `${searchPersonsUrl}?keyword=${keyword}`;
   $.getJSON(searchRequest, function(resp) {
-    console.log({ resp });
     if (resp.error) {
       // handle error
-      console.log("error occured", resp.message);
+      const errorMessage = `<li style="color:red">${resp.message}</li>`;
       $(resultsList)
-        .html(resp.message)
+        .html(errorMessage)
         .slideDown("slow");
       return false;
     }
-
-    persons = resp.data;
-    handleSearchResults(persons);
+    // pass data returned to function rendering autocomplete
+    return renderAutoComplete(resp.data);
   });
 }
-
-// upload csv
-function handleSearchResults(result) {
-  console.log({ result });
-  console.log("search len", result.length);
-  // render auto complete
-  const autoComplete = renderAutoComplete(result);
-}
-
-// upload csv
+// display autocomplete
 function renderAutoComplete(result) {
   if (result.length < 1) {
     // handle no result found
-    const message = "<li>No Records found</li>";
-    $(resultsList)
+    const message = `<li>No Records found</li>`;
+    return $(resultsList)
       .html(message)
       .slideDown("slow");
   }
@@ -114,12 +52,31 @@ function renderAutoComplete(result) {
       result.team
     }">${result.name}</li>`;
   });
+
   $(resultsList)
     .html(list)
     .slideDown("slow");
-  console.log({ list });
-}
 
+  // set click event listener for each item
+  const item = document.getElementsByClassName("search-result-item");
+  return Array.from(item).forEach(element => {
+    return element.addEventListener("click", handleSearchResultSelection);
+  });
+}
+// handle when search result is clicked
+function handleSearchResultSelection() {
+  // hide autocomplete
+  $(resultsList).slideUp("slow");
+  // get data
+  const data = this.dataset;
+  // render data
+  const renderedHtml = renderResultDetails(data);
+  // display data
+  return $(resultDetails)
+    .html(renderedHtml)
+    .show("slow");
+}
+// display result details
 function renderResultDetails(data) {
   return `<table class='table table-striped'><tbody><tr><td>Name</td><td>${
     data.name
@@ -127,3 +84,86 @@ function renderResultDetails(data) {
     data.address
   }</td></tr><tr><td>Team</td><td>${data.team}</td></tr></tbody></table>`;
 }
+
+function hideUploadMessages() {
+  // hide progress bar container
+  setTimeout(function() {
+    $(progressContainer).hide();
+    progressBar.style.width = 0;
+  }, 1000);
+  // hide upload message
+  setTimeout(function() {
+    $(uploadMessage).slideUp();
+  }, 3000);
+}
+
+// run when document is loaded
+$(document).ready(function() {
+  // handle search
+  $(searchField).on("keyup paste", function() {
+    // hide search item details element
+    $(resultDetails).slideUp();
+    const keyword = $(this).val();
+    if (keyword.length < 1) {
+      return $(resultsList).slideUp();
+    }
+    // search db with keyword
+    return searchPersons(keyword);
+  });
+
+  // handle csv uploads
+  $(uploadForm).on("submit", function(event) {
+    event.preventDefault();
+    // shift focus to the search box
+    // $(searchField).focus();
+    // hide message box
+    $(uploadMessage).hide();
+    // show progress bar
+    $(progressContainer).show();
+    // get file to be uploaded
+    const file = document.getElementById(uploadField).files[0];
+    // check if valid file type
+    if (!file.type.includes("csv")) {
+      const message = `<div class="alert alert-danger">You can only upload CSV files</div>`;
+      $(uploadMessage)
+        .html(message)
+        .show("slow");
+      return hideUploadMessages();
+    }
+
+    // create form data to be sent
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // create xhr request
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        const response = JSON.parse(xhr.response);
+        if (xhr.status === 200) {
+          // handle success
+          progressBar.style.backgroundColor = "green";
+          const successMessage = `<div class="alert alert-success">${
+            response.message
+          }</div>`;
+          return $(uploadMessage)
+            .html(successMessage)
+            .show("slow");
+        } else {
+          // handle error
+          progressBar.style.backgroundColor = "red";
+          const errorMessage = `<div class="alert alert-danger">${
+            response.message
+          }</div>`;
+          return $(uploadMessage)
+            .html(errorMessage)
+            .show("slow");
+        }
+      }
+      return hideUploadMessages();
+    };
+    xhr.upload.addEventListener("progress", showProgress);
+    xhr.open("POST", createPersonsUrl, true);
+    xhr.send(formData);
+  });
+});
--- src/server/routes/persons.js
+++ src/server/routes/persons.js
@@ -1,6 +1,8 @@
-var express = require("express");
-var router = express.Router();
-var csv = require("fast-csv");
+const express = require("express");
+const router = express.Router();
+const csv = require("fast-csv");
+const fs = require("fs");
+const path = require("path");
 const Person = require("../models/Person");
 
 /* return list of persons in db */
@@ -48,37 +50,108 @@ router.post("/create", function(req, res, next) {
 
   // get uploaded csv
   // console.log("files ", req.files);
-  var peopleFile = req.files.file;
+  const peopleFile = req.files.file;
+  const csvFolder = path.resolve("./src/server/public/csvs/");
+  const fileName = csvFolder + "/data.csv";
+  console.log({ csvFolder });
+  peopleFile.mv(fileName, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
   // console.log("people file ", peopleFile);
-  var people = [];
-  csv
-    .fromString(peopleFile.data.toString(), {
-      headers: false,
-      ignoreEmpty: true
-    })
+  const stream = fs.createReadStream(fileName);
+  // console.log({ stream });
+  // var people = [];
+  const csvStream = csv()
     .on("data", function(data) {
-      data["name"] = data[1];
-      data["age"] = data[2];
-      data["address"] = data[3];
-      data["team"] = data[4];
-      // console.log({ data });
-      people.push(data);
+      if (!data[1]) return false;
+      let fields = {};
+      fields["name"] = data[1];
+      fields["age"] = data[2];
+      fields["address"] = data[3];
+      fields["team"] = data[4];
+      console.log({ fields });
+      const person = new Person(fields);
+      person.save(function(err) {
+        if (err) console.log("error creating person", err);
+        console.log("success creating person");
+      });
+    })
+    .on("error", function(e) {
+      console.log("errrrrrrr", e);
+      return res.status(400).json({
+        error: true,
+        message: "An error occured parsing document"
+      });
     })
     .on("end", function() {
-      console.log("people ", people);
-      Person.create(people, function(err, documents) {
-        if (err)
-          return res.json({
-            error: true,
-            message: "An error occured creating persons"
-          });
+      // console.log("people ", people);
+      // if (people.length < 1) {
+      //   return res.status(400).json({
+      //     error: true,
+      //     message: "Uploaded file does not match expected format"
+      //   });
+      // }
 
-        return res.json({
-          success: true,
-          message: "Data saved successfully"
-        });
+      // if (err)
+      //   return res.status(400).json({
+      //     error: true,
+      //     message: "An error occured creating persons"
+      //   });
+
+      return res.json({
+        success: true,
+        message: "Data saved successfully"
       });
     });
+
+  stream.pipe(csvStream);
+
+  // csv
+  //   .fromString(peopleFile.data.toString(), {
+  //     headers: false,
+  //     ignoreEmpty: true
+  //   })
+  //   .on("data", function(data) {
+  //     if (!data[1]) return false;
+  //     data["name"] = data[1];
+  //     data["age"] = data[2];
+  //     data["address"] = data[3];
+  //     data["team"] = data[4];
+  //     console.log({ data });
+  //     people.push(data);
+  //   })
+  //   .on("error", function() {
+  //     console.log("errrrrrrr");
+  //     return res.status(400).json({
+  //       error: true,
+  //       message: "An error occured parsing document"
+  //     });
+  //   })
+  //   .on("end", function() {
+  //     console.log("people ", people);
+  //     if (people.length < 1) {
+  //       return res.status(400).json({
+  //         error: true,
+  //         message: "Uploaded file does not match expected format"
+  //       });
+  //     }
+
+  //     Person.create(people, function(err, documents) {
+  //       if (err)
+  //         return res.status(400).json({
+  //           error: true,
+  //           message: "An error occured creating persons"
+  //         });
+
+  //       return res.json({
+  //         success: true,
+  //         message: "Data saved successfully"
+  //       });
+  //     });
+  //   });
 });
 
 module.exports = router;
